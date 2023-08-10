03682d7d2a8a7ec0ecc8f34184575f8945b15aad
RaymondCM <ray.tunstill@live.co.uk>
2023-08-10 23:40:12 +0100

    Updated pypi push script

--- PythonAPI/push_to_pypi.sh
+++ PythonAPI/push_to_pypi.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 set -e
-pip install --upgrade setuptools wheel twine
+python3 -m pip install --upgrade setuptools wheel twine
 rm -r dist || true
-python setup.py sdist
-python -m twine upload dist/* || true
+python3 setup.py sdist
+python3 -m twine upload dist/* || true
 rm -r dist
